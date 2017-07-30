3f9ffa7b6910d76ae9271a7e7d43738a1a4b86af
Tareq Jobayere <tarexyz@gmail.com>
2017-07-30 13:46:27 +0600

    done

--- src/helper/utility/superFetch.js
+++ src/helper/utility/superFetch.js
@@ -1,18 +1,5 @@
 import config from '../../config';
 
-// const TVAPI = (endpoint, page = 1) => {
-//   return fetch(
-//     `${config.tmdb.url}/${endpoint}?api_key=${config.tmdb.key}&page=${page}&language=en-US`,
-//     {
-//       method: 'GET',
-//       mode: 'cors',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     },
-//   ).then(res => res.json());
-// };
-
 const getDate = (day = 0) => {
   if (day !== 0) {
     const previousDay = new Date() - 1000 * 60 * 60 * 24 * day;
@@ -21,7 +8,6 @@ const getDate = (day = 0) => {
   return new Date().toISOString().slice(0, 10);
 };
 
-// `${config.tvurl}/schedule?country=GB&date=${getDate(day)}
 
 const TVAPI = (day = 0) => {
   return fetch(`${config.tvurl}/schedule?country=CN&date=${getDate(day)}`, {
