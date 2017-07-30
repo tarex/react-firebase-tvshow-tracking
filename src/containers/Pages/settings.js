0bdc74ec1122984550b6cf98287335ae7275d983
Tareq Jobayere <tarexyz@gmail.com>
2017-07-30 13:33:07 +0600

    font preload

--- src/containers/Pages/settings.js
+++ src/containers/Pages/settings.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import Switch from 'react-toolbox/lib/switch/Switch';
-import Dropdown from 'react-toolbox/lib/dropdown/Dropdown';
+import Autocomplete from 'react-toolbox/lib/autocomplete/Autocomplete';
 import Button from 'react-toolbox/lib/button/Button';
+import moment from 'moment-timezone';
 
-const countries = [
-  { value: 'EN-gb', label: 'England' },
-  { value: 'ES-es', label: 'Spain' },
-  { value: 'TH-th', label: 'Thailand' },
-  { value: 'EN-en', label: 'USA' },
-];
+const source = {
+  'ES-es': 'Spain',
+  'TH-th': 'Thailand',
+  'EN-gb': 'England',
+  'EN-en': 'USA',
+};
 
 class Settings extends Component {
+  state = {
+    settings: {},
+  };
   handleChange = value => {
     console.log(value);
   };
@@ -19,12 +23,14 @@ class Settings extends Component {
       <div>
         <h1>Settings page</h1>
         <Switch label="Don't send push notifications" />
-        <Dropdown
-          auto
+        <Autocomplete
+          direction="down"
+          selectedPosition="above"
+          label="Choose countries"
           onChange={this.handleChange}
-          source={countries}
-          label="Select your timezone"
+          source={moment.tz.names()}
         />
+        <Switch label="Send faceboo1k notifications" />
         <Button label="Delete My account" raised accent />
 
       </div>
